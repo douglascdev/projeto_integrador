3f26469623798ec69d8ef212a6a744d2936d0596
douglas-cpp <douglasc.dev@gmail.com>
2020-07-31 16:20:45 -0300

    Adicionada a barra de menu

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import MenuAppBar from "./MenuAppBar";
 
 class App extends Component {
   constructor(props) {
@@ -7,41 +8,33 @@ class App extends Component {
     this.state = {
       data: [],
       loaded: false,
-      placeholder: "Loading"
+      placeholder: "Carregando..."
     };
   }
 
   componentDidMount() {
-    fetch("cadastro/caminhao")
-      .then(response => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Não foi possível carregar o cadastro!" };
-          });
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true
-          };
-        });
-      });
+    // fetch("cadastro/caminhao")
+    //   .then(response => {
+    //     if (response.status > 400) {
+    //       return this.setState(() => {
+    //         return { placeholder: "Não foi possível carregar o cadastro!" };
+    //       });
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(data => {
+    //     this.setState(() => {
+    //       return {
+    //         data,
+    //         loaded: true
+    //       };
+    //     });
+    //   });
   }
 
   render() {
     return (
-      <ul>
-        {this.state.data.map(contact => {
-          return (
-            <li key={contact.id}>
-              {contact.modelo} - {contact.marca}
-            </li>
-          );
-        })}
-      </ul>
+        <MenuAppBar/>
     );
   }
 }
